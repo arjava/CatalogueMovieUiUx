67d4a00b49616615c966193d0ab1902066123a5e
arjava <arjava379@gmail.com>
2017-11-17 17:27:42 +0300

    first commit

---
+++ app/src/main/java/com/arjava/dcodingcatmovieuiux/adapter/AdapterMovie.java
@@ -0,0 +1,10 @@
+package com.arjava.dcodingcatmovieuiux.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by arjava on 11/17/17.
+ */
+
+public class AdapterMovie extends RecyclerView {
+}
